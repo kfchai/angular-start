3ced6959f21861c50220236bd318c5d814b180a7
Chai Kit Fung <kfchai@gmail.com>
2016-12-01 12:25:09 +0800

    make additional boilerplate items

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const elixir = require('laravel-elixir');
 
+ elixir.config.publicPath = 'public';
  elixir(mix => {
  	mix.sass('app.scss')
  	.webpack('app.js');
---
+++ resources/assets/js/app.js
@@ -0,0 +1,10 @@
+
+window._ = require('lodash');
+
+window.$ = window.jQuery = require('jquery');
+
+require('bootstrap-sass');
+
+import angular from 'angular';
+
+angular.module('app', []);
